f0a39153ff50f3154553005c8a070eafd9d61903
Имя Фамилия <Ramis220400@gmail.com>
2022-02-10 14:49:04 +0300

    Окно добавления сотрудника, верстка закончена, незакончена валидация

--- RentOfEquipment/Windows/EmployeeListAdd.xaml.cs
+++ RentOfEquipment/Windows/EmployeeListAdd.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentOfEquipment.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,67 @@ namespace RentOfEquipment.Windows
         public EmployeeListAdd()
         {
             InitializeComponent();
+            cbRole.ItemsSource = ClassHelper.AppData.Context.Role.ToList();
+            cbRole.DisplayMemberPath = "Name";
+            cbRole.SelectedIndex = 0;
+
+
+        }
+
+        
+
+        private void btnEmployeeAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Пустые значения в поле Фамилия", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("Пустые значения в поле Имя", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Пустые значения в поле Телефон", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtLogin.Text))
+            {
+                MessageBox.Show("Пустые значения в поле логин", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtPassword.Password))
+            {
+                MessageBox.Show("Пустые значения в поле пароль", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+
+            try
+            {
+                EF.Employee newEmployee = new EF.Employee();
+                newEmployee.LastName = txtLastName.Text;
+                newEmployee.FirstName = txtFirstName.Text;
+                newEmployee.MiddleName = txtMiddleName.Text;
+                newEmployee.Phone = txtPhone.Text;
+                newEmployee.Login = txtLogin.Text;
+                newEmployee.Password = txtPassword.Password;
+                newEmployee.IdRole = (cbRole.SelectedItem as EF.Role).Id;
+                ClassHelper.AppData.Context.Employee.Add(newEmployee);
+                
+            }
+
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
