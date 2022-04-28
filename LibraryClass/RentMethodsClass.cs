4a4a0921cd163b13a5e962ebe3772cb0624cbede
laflafPow <tuzhilaflaf@gmail.com>
2022-04-28 17:49:04 +0600

    Methods

---
+++ LibraryClass/RentMethodsClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentOfEquipment;
+
+namespace LibraryClass
+{
+    public class RentMethodsClass
+    {
+        public static double RentSalary(DateTime StartRentDate, DateTime EndRentDate, RentOfEquipment.EF.Equipment equipment, int Qty)
+        {
+            int days = (StartRentDate - EndRentDate).Days;
+
+            double finishSalary = Qty * (Convert.ToDouble(equipment.Price) * 0.25 * days);
+
+            return finishSalary;
+        }
+    }
+}
