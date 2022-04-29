cbbd0ce4c0b220bacd72edfbdfebd70eabadce36
laflafPow <tuzhilaflaf@gmail.com>
2022-04-29 17:47:53 +0600

    RentMethod

--- LibraryClass/RentMethodsClass.cs
+++ LibraryClass/RentMethodsClass.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RentOfEquipment;
 
 namespace LibraryClass
 {
     public class RentMethodsClass
     {
-        public static double RentSalary(DateTime StartRentDate, DateTime EndRentDate, RentOfEquipment.EF.Equipment equipment, int Qty)
+        public static double RentSalary(DateTime StartRentDate, DateTime EndRentDate, ClassHelper.Equipment equipment, int Qty)
         {
             int days = (StartRentDate - EndRentDate).Days;
 
