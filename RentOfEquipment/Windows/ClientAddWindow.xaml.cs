7f59a790ca1bf16ad123816f03590fc764607e0a
Имя Фамилия <visualstudio1945@outlook.com>
2022-05-26 14:46:22 +0300

    Окно сотрудника закончено - окно клиента наполовину

---
+++ RentOfEquipment/Windows/ClientAddWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using RentOfEquipment.ClassHelper;
+
+namespace RentOfEquipment.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientAddWindow.xaml
+    /// </summary>
+    public partial class ClientAddWindow : Window
+    {
+        bool isEdit = false;
+        EF.Client editClient = new EF.Client();
+        string pathPhoto = null;
+
+        public ClientAddWindow()
+        {
+            InitializeComponent();
+            cbRole.ItemsSource = ClassHelper.AppData.Context.Role.ToList();
+            cbRole.DisplayMemberPath = "Name";
+            cbRole.SelectedIndex = 0;
+
+            isEdit = false;
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(email);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        public ClientAddWindow(EF.Client client)
+        {
+            InitializeComponent();
+            cbRole.ItemsSource = ClassHelper.AppData.Context.Role.ToList();
+            cbRole.DisplayMemberPath = "Name";
+
+            txtLastName.Text = client.LastName;
+            txtFirstName.Text = client.FirstName;
+            txtMiddleName.Text = client.MiddleName;
+            txtPhone.Text = client.Phone;
+            txtEmail.Text = client.Email;
+
+            // ?*@?*.?*
+
+            cbRole.SelectedIndex = client.IdGender - 1;
+
+            if (client.Photo != null)
+            {
+                using (MemoryStream stream = new MemoryStream(client.Photo))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                    bitmapImage.StreamSource = stream;
+                    bitmapImage.EndInit();
+
+                    photoUser.Source = bitmapImage;
+                }
+            }
+
+            tbTitle.Text = "Изменение данных работника";
+            btnEmployeeAdd.Content = "Сохранить";
+
+            isEdit = true;
+
+            editClient = client;
+        }
+
+
+        private void btnEmployeeAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (String.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Пустые значения в поле Фамилия", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("Пустые значения в поле Имя", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Пустые значения в поле Телефон", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtLogin.Text))
+            {
+                MessageBox.Show("Пустые значения в поле логин", "Erorr", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!Int32.TryParse(txtPhone.Text, out int res))
+            {
+                MessageBox.Show("В поле телефон возможны только цифры", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (txtEmail.Text != "")
+            {
+                if (IsValidEmail(txtEmail.Text) == false)
+                {
+                    MessageBox.Show("E-mail не соответсвует маске (?*@?*.?*)", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+
+            if (isEdit)
+            {
+                var resClick = MessageBox.Show("Изменить пользователя?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (resClick == MessageBoxResult.No)
+                {
+                    return;
+                }
+
+                try
+                {
+                    editClient.LastName = txtLastName.Text;
+                    editClient.FirstName = txtFirstName.Text;
+                    editClient.MiddleName = txtMiddleName.Text;
+                    editClient.Phone = txtPhone.Text;
+                    editClient.Email = txtEmail.Text;
+
+                    // ?*@?*.?*
+
+                    editClient.IdGender = (cbRole.SelectedItem as EF.Gender).Id;
+
+                    if (pathPhoto != null)
+                    {
+                        editClient.Photo = File.ReadAllBytes(pathPhoto);
+                    }
+
+                    ClassHelper.AppData.Context.SaveChanges();
+
+                    MessageBox.Show("Пользователь изменен");
+                    this.Close();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            else
+            {
+                var resClick = MessageBox.Show("Добавить пользователя?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (resClick == MessageBoxResult.No)
+                {
+                    return;
+                }
+
+                try
+                {
+                    EF.Employee newEmployee = new EF.Employee();
+
+                    newEmployee.LastName = txtLastName.Text;
+                    newEmployee.FirstName = txtFirstName.Text;
+                    newEmployee.MiddleName = txtMiddleName.Text;
+                    newEmployee.Phone = txtPhone.Text;
+                    newEmployee.Email = txtEmail.Text;
+                    newEmployee.IdRole = (cbRole.SelectedItem as EF.Role).Id;
+                    newEmployee.Login = txtLogin.Text;
+                    newEmployee.Password = txtPassword.Password;
+
+                    if (pathPhoto != null)
+                    {
+                        newEmployee.Photo = File.ReadAllBytes(pathPhoto);
+                    }
+
+                    ClassHelper.AppData.Context.Employee.Add(newEmployee);
+                    ClassHelper.AppData.Context.SaveChanges();
+
+                    MessageBox.Show("Пользователь добавлен");
+
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+
+        }
+
+        private void btnChoosePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Files|*.jpg;*.jpeg;*.png;";
+            if (openFile.ShowDialog() == true)
+            {
+                photoUser.Source = new BitmapImage(new Uri(openFile.FileName));
+                pathPhoto = openFile.FileName;
+            }
+        }
+    }
+}
--- RentOfEquipment/Windows/ClientListWindow.xaml.cs
+++ RentOfEquipment/Windows/ClientListWindow.xaml.cs
@@ -19,10 +19,126 @@ namespace RentOfEquipment.Windows
     /// </summary>
     public partial class ClientListWindow : Window
     {
+        List<string> listSort = new List<string>()
+        {
+            "По умолчанию",
+            "По фамилии",
+            "По имени",
+            "По электронной почте",
+            "По полу",
+        };
+
         public ClientListWindow()
         {
             InitializeComponent();
-            lvClient.ItemsSource = ClassHelper.AppData.Context.Client.ToList();
+            Filter();
+            cbSort.ItemsSource = listSort;
+            cbSort.SelectedIndex = 0;
+        }
+
+        private void Filter()
+        {
+            List<EF.Client> listClient = new List<EF.Client>();
+
+            listClient = ClassHelper.AppData.Context.Client.ToList();
+
+            listClient = listClient.
+                Where(i => i.LastName.ToLower().Contains(txtSearch.Text.ToLower())
+                || i.FirstName.ToLower().Contains(txtSearch.Text.ToLower())
+                || i.MiddleName.ToLower().Contains(txtSearch.Text.ToLower())
+                || i.FIO.ToLower().Contains(txtSearch.Text.ToLower())
+                || i.Phone.ToLower().Contains(txtSearch.Text.ToLower())
+                || i.Email.ToLower().Contains(txtSearch.Text.ToLower()) 
+                || (i.IsDeleted != false)).
+                ToList();
+
+            switch (cbSort.SelectedIndex)
+            {
+                case 0:
+                    listClient = listClient.OrderBy(i => i.Id).ToList();
+                    break;
+
+                case 1:
+                    listClient = listClient.OrderBy(i => i.LastName).ToList();
+                    break;
+
+                case 2:
+                    listClient = listClient.OrderBy(i => i.FirstName).ToList();
+                    break;
+
+                case 3:
+                    listClient = listClient.OrderBy(i => i.Email).ToList();
+                    break;
+
+                case 4:
+                    listClient = listClient.OrderBy(i => i.IdGender).ToList();
+                    break;
+
+                default:
+                    listClient = listClient.OrderBy(i => i.Id).ToList();
+                    break;
+            }
+
+            lvClient.ItemsSource = listClient;
+        }
+
+        private void btnAddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            ClientAddWindow clientAddWindow = new ClientAddWindow();
+            clientAddWindow.ShowDialog();
+            Filter();
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void lvClient_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete || e.Key == Key.Back)
+            {
+                var resClick = MessageBox.Show("Удалить пользователя?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (resClick == MessageBoxResult.No)
+                {
+                    return;
+                }
+
+                try
+                {
+                    if (lvClient.SelectedItem is EF.Client)
+                    {
+                        var client = lvClient.SelectedItem as EF.Client;
+                        client.IsDeleted = true;
+                        ClassHelper.AppData.Context.SaveChanges();
+                        MessageBox.Show("Пользователь успешно удален", "Готово", MessageBoxButton.OK, MessageBoxImage.Information);
+                        Filter();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void lvClient_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lvClient.SelectedItem is EF.Client)
+            {
+                var client = lvClient.SelectedItem as EF.Client;
+
+                ClientAddWindow clientAddWindow = new ClientAddWindow(client);
+                clientAddWindow.ShowDialog();
+                Filter();
+
+            }
         }
     }
 }
